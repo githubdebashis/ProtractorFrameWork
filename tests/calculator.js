41f20bae6bcbff3091082c4f4fd710cb7a77ad9d
githubdebashis <Debashisdey.2010@gmail.com>
2020-04-13 13:00:34 +0530

    Updated on 13th Apr Calcultor.js

--- tests/calculator.js
+++ tests/calculator.js
@@ -34,5 +34,39 @@ it('Addition Test', function(){
 
 });
 
+it('Subtraction Test', function(){
+
+  //  browser.get('http://juliemr.github.io/protractor-demo/');
+
+     homepage.get('http://juliemr.github.io/protractor-demo/');
+
+    //element(by.model('first')).sendKeys('2');
+
+    homepage.enterFirstNumber('5');
+
+
+// element(by.model('second')).sendKeys('4');
+
+  homepage.enterSecondNumber('4');
+
+
+  // element(by.css('[ng-click="doAddition()"]')).click();
+
+   homepage.clickGo1();
+
+  // let result =  element(by.cssContainingText('.ng-binding', '6'));
+
+   //expect(result.getText()).toEqual('6');
+
+   homepage.verifyResult('9');
+
+    browser.sleep(2000);
+    
+
+});
+
+
+
+
 
 });
